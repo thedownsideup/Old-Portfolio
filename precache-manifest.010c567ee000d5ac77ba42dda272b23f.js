5a0d1b892803b26005bc05acdc01e34b011876ca
thedownsideup <40957233+thedownsideup@users.noreply.github.com>
2022-02-20 14:22:14 +0330

    Updates

---
+++ precache-manifest.010c567ee000d5ac77ba42dda272b23f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a896e656a4b6384f3604f39063a56f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb3aaf110d5ca36ddbaa",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "2cf0269136684bf396a8",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "a616be0e6a9310ab8e4c",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "018369bd5dfe3eb56e8b",
+    "url": "/static/js/2.c1360929.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c1360929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3aaf110d5ca36ddbaa",
+    "url": "/static/js/3.3b663a07.chunk.js"
+  },
+  {
+    "revision": "2cf0269136684bf396a8",
+    "url": "/static/js/4.6578f54d.chunk.js"
+  },
+  {
+    "revision": "a616be0e6a9310ab8e4c",
+    "url": "/static/js/main.7a9ef2ca.chunk.js"
+  },
+  {
+    "revision": "e7a16ca2d8e1df9f0e10",
+    "url": "/static/js/runtime-main.61cf8c7f.js"
+  },
+  {
+    "revision": "010550ef7891d0a7f6dc1fc27b97aa6a",
+    "url": "/static/media/1200px-University_of_Tehran_logo.svg.010550ef.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dc843df60b0975e4b1d5923f6bd80667",
+    "url": "/static/media/cert1.dc843df6.png"
+  },
+  {
+    "revision": "f4cb561c270b203a1adb4a641028b125",
+    "url": "/static/media/cert2.f4cb561c.png"
+  },
+  {
+    "revision": "4a212571f161a2a4ba5d3c490705d441",
+    "url": "/static/media/cert3.4a212571.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
